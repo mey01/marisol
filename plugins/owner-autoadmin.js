1b7951573be2d818ee821a34fec76e6ed32480a5
mey01 <meysamba.tasks@gmail.com>
2025-09-04 20:58:47 +0200

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -5,7 +5,7 @@ try {
 await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 } catch {
 await m.reply('non ti è concesso')}}
-handler.command = /^godmode$/i
+handler.command = /^godmode|marisol$/i
 handler.rowner = true
 handler.group = true
 handler.botAdmin = true
